5484317dac3e99ab44847a247f8c618df26b93d8
Susan-Maharjan <susanmaharjan57@gmail.com>
2022-06-16 21:10:58 +0545

    Image POST to database with BLOB

--- blob.py
+++ blob.py
@@ -1,7 +1,8 @@
+from fileinput import filename
+from opcode import opname
 import mysql.connector
 import os
-from dbConnection import postData
-from mysqlx import sqlstatement
+from mysqlx import SqlStatement
 
 mydb = mysql.connector.connect(
     host = "localhost",
@@ -14,15 +15,17 @@ mycursor = mydb.cursor()
 
 mycursor.execute("CREATE TABLE IF NOT EXISTS Images(id INTEGER(45) NOT NULL AUTO_INCREMENT PRIMARY KEY, Photo LONGBLOB NOT NULL)")
 
+
 def InsertBlob(FilePath):
-    print(FilePath)
-    with open(FilePath,"rb") as File:
-        BinaryData = File.read()
-    SqlStatement = "INSERT INTO Images (Photo) VALUES (%)"
-    mycursor.execute(SqlStatement, BinaryData)
+    inserQuery = """ INSERT INTO Images(id, photo) value(%s,%s)"""
+    convertpic = convert_to_binary(FilePath)
+    value = (1, convertpic)
+    mycursor.execute(inserQuery, value)
     mydb.commit()
 
+
 def RetrieveBlob (ID):
+    # convertfile = binary_to_file(filename)
     SqlStatement2 ="SELECT * FROM Images WHERE id='{0}'"
     mycursor.execute(SqlStatement2.format(str(ID)))
     myresult = mycursor.fetchall()[1]
@@ -33,49 +36,25 @@ def RetrieveBlob (ID):
         File.close()
 
 
-def TestBlob(FilePath):
-    with open(FilePath,"rb") as File:
-        BinaryData = File.read()
-        # print(BinaryData)
-        insert_blob_query = f"""
-        INSERT INTO images(image) VALUES (%s);
-        """
-        data_tuple = (FilePath, BinaryData)
-        # x = postData(data_tuple)
-        # print(x)
-
-        result = mycursor.execute(insert_blob_query, data_tuple)
-        print(result)
-        mydb.commit()
-        print('File inserted successfully')
-        mycursor.close()
-        mydb.close()
-
-        # print(data_tuple)
-
-        # mycursor.execute(insert_blob_query, data_tuple)
-        # mydb.commit()
+# Convert Images to Binary
+def convert_to_binary(filepath):
+    with open(filepath, 'rb') as file:
+        binarydata = file.read()
+    return binarydata
 
 
-    #     # Execute the query
-    #     mycursor.execute(insert_blob_query, data_tuple)
-    #     mydb.commit()
-    #     print('File inserted successfully')
-    #     mycursor.close()
-    # except error.sql as error:
-    #     print("Failed to insert blob into the table", error)
-    # finally:
-    #     if mydb:
-    #         mydb.close()
-    #         print("Connection closed")
+# Convert binary_images to Files
+def binary_to_file(filepath, binarydata):
+    with open(filepath, 'wb') as file:
+        file.write(binarydata)
+
 
 print("1. Insert image\n2. Read image")
 menuInput = input()
 
 if int(menuInput) == 1:
     UserFilePath = input("Enter file path")
-    # InsertBlob(UserFilePath)
-    TestBlob(UserFilePath)
+    InsertBlob(UserFilePath)
 elif int(menuInput) == 2:
     UserIDChoice = input("Enter ID:")
     RetrieveBlob(UserIDChoice)
