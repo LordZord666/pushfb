f18e55a92c1a202913f5512690f60b7e141b02ef
Jordan Maharjan <70703500+LordZord666@users.noreply.github.com>
2022-06-23 13:31:37 +0545

    Update tkpush.py

--- tkpush.py
+++ tkpush.py
@@ -2,6 +2,7 @@ import facebook
 from dbConnection import getData
 import schedule
 import time
+import requests
 
 page_access_token = "EAAF219B1ahEBAEPKtkgJVpnOBNVajj63uoi7kazXZBNBVHfuYVrd6J2CPpj3PGaWgq1SVe5ZCqDZB5EqW4sJGDMfFY4tberZAMy2rkSRppTgcMSMOseUF9zZBWeES7tXHT8d1SPwVO8K2JpZAzXDETnXXUTywPjfjUw0wsKz6xKGlkYqKqewri"
 graph = facebook.GraphAPI(page_access_token)
@@ -12,11 +13,21 @@ def job():
   datas = getData()
   message = ''
   for data in datas:
-    message += str(data[0])+" "+str(data[1])+" "+str(data[2])+" "+str(data[3])+'\n'  
+    message += str(data[0])+" "+str(data[1])+" "+str(data[2])+" "+str(data[3])+" "+str(data[4])+" "+str(data[5])+'\n'  
   graph.put_object(facebook_page_id, "feed", message=message)
   print("Posting completed")
 
-schedule.every(5).seconds.do(job)
-while True:
-  schedule.run_pending()
-  time.sleep(1)
+# image_url = 'https://graph.facebook.com/{}/photos'.format(facebook_page_id)
+# image_location = ''
+# img_payload = {
+# 'url': image_location,
+# 'access_token': page_access_token
+# }
+# r = requests.post(image_url, data=img_payload)
+# print(r.text)
+  
+
+# schedule.every(5).seconds.do(job)
+# while True:
+#   schedule.run_pending()
+#   time.sleep(1)
